3cc2ea8ea78d71110c90830e7bb936db3998474d
carlo delgado <cdmarallano23@gmail.com>
2024-03-21 21:48:36 -0500

    chore[8]: priomer commit despues de tiempo

--- src/main/java/hackathon/nttdata/coderpath/preguntas/PreguntasApplication.java
+++ src/main/java/hackathon/nttdata/coderpath/preguntas/PreguntasApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class PreguntasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PreguntasApplication.class, args);
+		SpringApplication.run(PreguntasApplication.class, args); 
 	}
 
 }
